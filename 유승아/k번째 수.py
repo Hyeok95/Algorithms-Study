653ae9f5fd381c5d4bd366db178dc46c3dcf3697
유승아 <93904773+yooseung20@users.noreply.github.com>
2021-12-27 17:17:37 +0900

    Add files via upload

---
+++ 유승아/k번째 수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+    for list in commands:
+        
+        i, j, k = list[0], list[1], list[2]
+        temp = array[i-1:j]
+        temp.sort()
+        answer.append(temp[k-1])        
+        
+    
+    return answer
