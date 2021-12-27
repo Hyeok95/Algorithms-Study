4ee9581145dc1ef9f2e27039f51ed55ea2432cf4
JSB1010 <seungbeomjang93@gmail.com>
2021-12-27 21:23:56 +0900

    commit

---
+++ 유승아/신규아이디 추천.Py
@@ -0,0 +1,28 @@
+def solution(new_id):
+    answer = ''
+    # 1단계
+    new_id = new_id.lower()
+    # 2단계
+    for i in new_id:
+        if i.isalnum() or i in ['-','_','.']:
+            answer += i
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4단계
+    if answer[0] == '.' and len(answer) > 1:
+        answer = answer[1:]  
+    if answer[-1] == '.' :
+        answer = answer[:-1] 
+    # 5단계
+    if answer == '':
+        answer = 'a' 
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7단계
+    if len(answer) <= 3:
+        answer += answer[-1] * (3-len(answer))
+    return answer
