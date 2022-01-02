060d970c9296abbb4721d4dcfb30732d9d2aadc1
JSB1010 <seungbeomjang93@gmail.com>
2022-01-02 18:57:04 +0900

    최대공약수와 최소공배수 풀이(추가)

--- 장승범/6주차/최대공약수와 최소공배수.py
+++ 장승범/6주차/최대공약수와 최소공배수.py
@@ -3,3 +3,15 @@ import math
 def solution(n, m):
     
     return [math.gcd(n, m), (n * m) // math.gcd(n, m)]
+
+
+'''
+def solution(n, m):
+    
+    a, b = max([n, m]), min([n, m])
+    
+    while b:
+        a, b = b, a % b
+    
+    return[a, int(n * m / a)]
+'''
