b55055a93237c760ea483745e62685a9c25cfeba
seungbeom1010 <seungbeom.dev@gmail.com>
2022-01-12 23:38:44 +0900

    완주하지 못한 선수(collections 사용풀이 추가)

--- 장승범/Level 1/10주차/완주하지 못한 선수.py
+++ 장승범/Level 1/10주차/완주하지 못한 선수.py
@@ -1,4 +1,14 @@
+'''
+collections 사용
 
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    
+    return list(answer.keys())[0]
+
+'''
 def solution(participant, completion):
     
     participant.sort()
