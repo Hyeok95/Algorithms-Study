20a6f94737bff4b3350a5bd2b1d9a89ca5c9e3c9
seungbeom1010 <seungbeom.dev@gmail.com>
2022-01-12 23:50:28 +0900

    10주차 풀이 및 11주차 폴더 추가

--- 장승범/Level 1/10주차/폰켓몬.py
+++ 장승범/Level 1/10주차/폰켓몬.py
@@ -1,1 +1,13 @@
+import collections
 
+def solution(nums):
+    
+    cns = int(len(nums) / 2)
+    
+    nums = list(collections.Counter(nums))
+    
+    if cns <= len(nums):
+        return cns
+    else:
+        return len(nums)
+    
