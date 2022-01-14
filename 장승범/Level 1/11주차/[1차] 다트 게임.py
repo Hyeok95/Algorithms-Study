51069fddbc651282678a281bc90439267abc34ca
seungbeom1010 <seungbeom.dev@gmail.com>
2022-01-14 16:25:41 +0900

    11주차 풀이 및 2022 KAKAO 문제 추가

--- 장승범/Level 1/11주차/[1차] 다트 게임.py
+++ 장승범/Level 1/11주차/[1차] 다트 게임.py
@@ -1,1 +1,30 @@
 
+def solution(dartResult):
+    
+    answer = []
+    
+    score_dict = {'S': 1, 'D': 2, 'T': 3}
+    
+    for i in range(len(dartResult)):
+        try:
+            try:
+                if type(int(dartResult[i - 1])) == int:
+                    answer[-1] = answer[-1] * 10 + int(dartResult[i])
+                else:
+                    answer.append(int(dartResult[i]))
+            except:
+                answer.append(int(dartResult[i]))
+            
+        except:
+            if dartResult[i] in score_dict:
+                answer[-1] = answer[-1] ** score_dict[dartResult[i]]
+            elif dartResult[i] == '*':
+                if len(answer) == 1:
+                    answer[-1] = answer[-1] * 2
+                else:
+                    answer[-1] = answer[-1] * 2
+                    answer[-2] = answer[-2] * 2
+            else:
+                answer[-1] = answer[-1] * (-1)
+    
+    return sum(answer)
--- 장승범/Level 1/11주차/약수의 개수와 덧셈.py
+++ 장승범/Level 1/11주차/약수의 개수와 덧셈.py
@@ -1,1 +1,14 @@
 
+def solution(left, right):
+    
+    answer = 0
+    
+    for i in range(left, right + 1):
+        cnt = 0
+        if i % (i ** 0.5) == 0:
+            answer -= i
+        else:
+            answer += i
+
+                
+    return answer
