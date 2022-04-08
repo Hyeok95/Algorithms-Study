391f28df7138df9a6f22baec490b5aebf560e8b7
Hyeok95 <chahn9503@nate.com>
2022-04-08 13:25:02 +0900

    메뉴리뉴얼 풀이

---
+++ 안창혁/Programmers/11주차/메뉴리뉴얼.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    
+    for num in course:
+        array = []
+        for order in orders:
+            order = sorted(order)
+            array.extend(list(combinations(order, num)))
+            
+        count = Counter(array)
+        print(count)
+        if count:
+            if max(count.values()) >= 2:
+                for key, value in count.items():
+                    if value == max(count.values()):
+                        answer.append("".join(key))
+    return sorted(answer)
